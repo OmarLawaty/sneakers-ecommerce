7e71b9444049268c6f3179659333048038e611b3
omarlawaty <omarlawatey@gmail.com>
2022-12-27 18:26:06 +0200

    Feat: setup Client

---
+++ Client/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { Container } from '@chakra-ui/react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Header } from './components';
+import { Home, Product, ProductNotFound, AddProduct } from './pages';
+
+const App = () => {
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
+
+  const setCartItem = (newItem = null, clearItem = false, clearCart = false) => {
+    let updateItemIndex = null;
+    let filteredCart = cart.filter((cartItem, index) => {
+      if (newItem.ID === cartItem.ID) updateItemIndex = index;
+
+      return cartItem.ID !== newItem.ID;
+    });
+
+    if (clearCart) {
+      setCart([]);
+      localStorage.setItem('cart', JSON.stringify([]));
+      return;
+    }
+
+    if (clearItem) {
+      setCart(filteredCart);
+      localStorage.setItem('cart', JSON.stringify(filteredCart));
+      return;
+    }
+    if (newItem === null) return;
+
+    if (updateItemIndex === null) {
+      setCart([...cart, newItem]);
+      localStorage.setItem('cart', JSON.stringify([...cart, newItem]));
+    }
+    if (typeof updateItemIndex === 'number') {
+      cart[updateItemIndex].amount = cart[updateItemIndex].amount + newItem.amount;
+      setCart([...filteredCart, cart[updateItemIndex]]);
+
+      localStorage.setItem('cart', JSON.stringify([...filteredCart, cart[updateItemIndex]]));
+    }
+  };
+
+  return (
+    <>
+      <Header cart={cart} setCartItem={setCartItem} />
+
+      <Container
+        as="main"
+        display="flex"
+        flexDir={['column', null, 'row']}
+        justifyContent="space-between"
+        px={['0', null, '16']}
+        py={['0', null, '20']}
+        gap={[null, null, '12', '20']}
+        mb="10"
+      >
+        <Routes>
+          <Route path="*" element={<Navigate to={`/products/`} />} />
+          <Route path="/products/add-product/" element={<AddProduct />} />
+          <Route path="/products/" element={<Home />} />
+          <Route path="/products/notfound" element={<ProductNotFound />} />
+          <Route path={`/products/:id`} element={<Product cart={cart} setCartItem={setCartItem} />} />
+        </Routes>
+      </Container>
+    </>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,70 @@
+# E-commerce product page
+
+![Desktop Preview](./design/desktop-preview.jpg)
+
+This is a solution to the [E-commerce product page](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The Challenge](#the-challenge)
+  - [Links](#links)
+- [Getting started](#getting-started) -[Start the Project](#start-the-project)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Links
+
+- [Solution URL](https://www.frontendmentor.io/solutions/ecommerce-product-page-uP_V8a_yD)
+- [Live Site URL](https://ecommerce-product-preview.vercel.app/)
+
+## Getting started
+
+### Start the project
+
+Make sure to have `node.js` `npm` and `yarn` installed and that you are in the root directory of the project, then simply run:
+
+```bash
+yarn install
+```
+
+To run the development server, execute:
+
+```bash
+yarn start
+```
+
+## My process
+
+### Built with
+
+- [React](https://reactjs.org/) - JS library
+- [Chakra-UI](https://chakra-ui.com) - Css component library
+- [React Router](https://reactrouter.com/) - Client-side routing
+
+### Useful resources
+
+- [W3schools](w3schools.com/)
+- [stackoverflow](stackoverflow.com/)
+- [MDN Web Docs](https://developer.mozilla.org/en-US/)
+
+## Author
+
+- Name - Omar Mohamed
+- Frontend Mentor - [@OmarLawaty](https://www.frontendmentor.io/profile/OmarLawaty)
+- Twitter - [@OmarLawaty](https://twitter.com/OmarLawaty)

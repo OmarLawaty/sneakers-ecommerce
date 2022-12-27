79420ba3d7b76737785f6b916e64fa6db0e73ddd
omarlawaty <omarlawatey@gmail.com>
2022-12-27 18:11:10 +0200

    Feat: Get all products endpoint

--- Server/app.py
+++ Server/app.py
@@ -8,5 +8,22 @@ app = Flask(__name__)
 db = SQL("sqlite:///database.db")
 
 
+@app.route('/products')
+def products():
+    products = db.execute('SELECT * FROM products')
+    prices = db.execute("SELECT * FROM prices")
+    images = db.execute("SELECT * FROM images")
+
+    for product in products:
+        for price in prices:
+            if product["ID"] == price["PRODUCT_ID"]:
+                product["PRICE"] = price
+        for image in images:
+            if product["ID"] == image["PRODUCT_ID"]:
+                product["IMAGES"] = image
+
+    return {"products": products}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
